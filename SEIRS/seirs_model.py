1540d13ec2ad1041fa43ac888c94de81d3b4af66
Ferran Parés <ferran.pares.pont@gmail.com>
2020-07-25 13:07:10 +0200

    initial version seirs_prediction

---
+++ SEIRS/seirs_model.py
@@ -0,0 +1,14 @@
+from seirsplus.models import *
+
+params = {
+    'beta': 0.155,     # Rate of transmission
+    'sigma': 1/5.2,    # Rate of progression
+    'gamma': 1/12.39,  # Rate of recovery
+    'xi': 0.001        # Rate of re-susceptibility
+}
+
+
+def seirs_prediction(initI, initN, **params):
+    model = SEIRSModel(initN=initN, initI=initI, **params)
+    model.run(T=15)
+    return model.total_num_infections()[10::10]
